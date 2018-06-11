4766cc4aa5477660fe078babe3d6eb3f687ea98b
paula tenorio <kari.962012@hotmail.com>
2018-06-11 17:59:40 -0500

    commit

--- app/src/main/java/com/example/paulatenorio/hojac/DatosPersonal.java
+++ app/src/main/java/com/example/paulatenorio/hojac/DatosPersonal.java
@@ -9,5 +9,6 @@ public class DatosPersonal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.DatosPersonales);
+
     }
 }
